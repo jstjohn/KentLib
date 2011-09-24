6ed264be75e192c05092d36f251d7f148e4036f3
John St. John <johnthesaintjohn@gmail.com>
2011-09-24 13:58:24 -0700

    adding script for copying over kent source subset to keep it up to date

---
+++ update_from_kent_source.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+KENTSRC=$HOME/kent/src
+
+pushd inc
+for F in `ls *.h`
+do
+    cp ${KENTSRC}/inc/${F} ${F}
+done
+
+popd
+
+
+pushd lib
+for F in `ls *.c`
+do
+    cp ${KENTSRC}/lib/${F} ${F}
+done
+popd
+
+make clean
+make
