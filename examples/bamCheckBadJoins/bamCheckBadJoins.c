fb735b7a1ba4ff6e30d73e97d44b0fcc85e18f49
John St. John <johnthesaintjohn@gmail.com>
2012-02-19 08:59:56 -0800

    bug fixes to bam example

--- examples/bamCheckBadJoins/bamCheckBadJoins.c
+++ examples/bamCheckBadJoins/bamCheckBadJoins.c
@@ -33,6 +33,7 @@
 #define DEFAULT_WINDOW 500
 #define DEFAULT_EDGES 4000
 #define DEFAULT_MQ 30
+#define MAX_OCOUNT_LEN 301
 
 
 
@@ -80,7 +81,51 @@ static struct optionSpec options[] = {
  *
  */
 
+int compare_unsigned (const unsigned *a, const unsigned *b)
+{
+  unsigned temp = *a - *b;
+  if (temp > 0)
+    return 1;
+  else if (temp < 0)
+    return -1;
+  else
+    return 0;
+}
 
+void setAdd(unsigned *lst, unsigned mtid){
+  if(lst[0] >= 1){
+    //search after the first element of the list for mtid
+    unsigned *rlst = bsearch(mtid, lst+1, lst[0], sizeof(unsigned), compare_unsigned );
+    if(rlst == NULL && lst[0] < MAX_OCOUNT_LEN){
+      //insert the new element into the list at the current end of the list
+      lst[lst[0]+1] = mtid;
+      lst[0]++;
+      //now re-sort the list after the first element
+      qsort(lst+1, lst[0], sizeof(unsigned), compare_unsigned);
+
+    }
+  }else{ //base case
+    lst[1] = mtid;
+    lst[0] = 1;
+  }
+}
+
+
+inline unsigned ** allocSetLst(int length){
+  int i = 0;
+  unsigned ** setList = (unsigned **) malloc(length * sizeof(unsigned *));
+  for(i=0;i<length;i++){
+    setList[i] = (unsigned *) calloc(MAX_OCOUNT_LEN, sizeof(unsigned));
+  }
+}
+
+inline void freeSetLst(unsigned **setLst, int length){
+  int i = 0;
+  for(i=0;i<length;i++){
+    free(setLst[i]);
+  }
+  free(setLst);
+}
 
 
 int bamPrintInfo(samfile_t *bamFile, FILE* out, int edges, int window, unsigned short minmq)
@@ -90,6 +135,7 @@ int bamPrintInfo(samfile_t *bamFile, FILE* out, int edges, int window, unsigned 
   bam_header_t *header = bamFile->header;
   unsigned short *bucket_counts_to_other = NULL;
   unsigned short *bucket_counts = NULL;
+  unsigned **bucket_num_other = NULL;
   boolean skipTID = TRUE;
   int numBuckets = 0;
   unsigned length = 0;
@@ -104,7 +150,7 @@ int bamPrintInfo(samfile_t *bamFile, FILE* out, int edges, int window, unsigned 
         char *name = header->target_name[lastTID];
 
         for(i=0;i<numBuckets;i++)
-          fprintf(out, "%s\t%d\t%hu\t%hu\n", name, (window * i) + edges, bucket_counts_to_other[i], bucket_counts[i] );
+          fprintf(out, "%s\t%d\t%hu\t%u\t%hu\n", name, (window * i) + edges, bucket_counts_to_other[i], bucket_num_other[i][0], bucket_counts[i] );
 
 
 
@@ -113,6 +159,8 @@ int bamPrintInfo(samfile_t *bamFile, FILE* out, int edges, int window, unsigned 
         bucket_counts_to_other = NULL;
         free(bucket_counts);
         bucket_counts = NULL;
+        freeSetLst(bucket_num_other, numBuckets);
+        bucket_num_other = NULL;
       }
 
       lastTID = b->core.tid;
@@ -134,9 +182,8 @@ int bamPrintInfo(samfile_t *bamFile, FILE* out, int edges, int window, unsigned 
       //calloc should 0 out arrays
       bucket_counts_to_other = (unsigned short *) calloc(numBuckets, sizeof(unsigned short));
       bucket_counts = (unsigned short *) calloc(numBuckets, sizeof(unsigned short));
-
-
-
+      bucket_num_other = allocSetLst(numBuckets);
+   
     }
     if(skipTID == TRUE)
       continue;
@@ -154,11 +201,15 @@ int bamPrintInfo(samfile_t *bamFile, FILE* out, int edges, int window, unsigned 
       if(bucket_idx >= numBuckets)
         continue;
 
-      bucket_counts[bucket_idx] += 1;
+      bucket_counts[bucket_idx]++;
 
       if(b->core.mtid != b->core.tid){
         //the mate is mapped to a different sequece
-        bucket_counts_to_other[bucket_idx] += 1;
+        bucket_counts_to_other[bucket_idx]++;
+
+        //add this mtid to the set of mtids for this bucket
+        setAdd(bucket_num_other[bucket_idx], mtid);
+
       }
 
     }//end bucket incrementation
