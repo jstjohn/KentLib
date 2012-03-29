11adff731db3eb5a3303e0da2c4c09b66b1eef74
John St. John <johnthesaintjohn@gmail.com>
2012-03-29 08:02:23 -0700

    pthread actually needed

--- examples/include.mk
+++ examples/include.mk
@@ -1,2 +1,2 @@
 CFLAGS+=-I../../thirdparty/samtools -I../../thirdparty/uthash/src -I../../inc -DUSE_BAM=1 -DHASH_FUNCTION=HASH_SFH
-LDFLAGS+=-L../.. -L../../thirdparty/samtools -lkent -lbam -lm -lz
+LDFLAGS+=-L../.. -L../../thirdparty/samtools -pthread -lkent -lbam -lm -lz
